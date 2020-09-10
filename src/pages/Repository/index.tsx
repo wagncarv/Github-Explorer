1d1d15c1ea2c6b491496cdae03102ddc9524cc6c
wagner carvalho <wagncarv@gmail.com>
2020-09-10 00:55:35 -0300

    criacao estrutura e estilos pagina repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams{
   repository: string;
@@ -8,7 +13,55 @@ interface RepositoryParams{
 const Repository: React.FC = () => {
 
   const { params } = useRouteMatch<RepositoryParams>();
-return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars3.githubusercontent.com/u/15336030?s=400&u=d3b19576227d5d649698263c3f82bc5e6231c742&v=4" alt="Imagem"/>
+          <div>
+            <strong>wagncarv/identicon</strong>
+            <p>Descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="abc">
+          <div>
+            <strong>Mons</strong>
+            <p>collis</p>
+          </div>
+
+          <FiChevronLeft size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
